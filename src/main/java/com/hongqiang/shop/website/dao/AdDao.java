0730a0b66c7dd68590cc0a399b643fbb5d9893cc
jiangyang118 <jiangyang118@163.com>
2013-12-20 22:55:12 +0800

    添加ad部分

---
+++ src/main/java/com/hongqiang/shop/website/dao/AdDao.java
@@ -0,0 +1,23 @@
+package com.hongqiang.shop.website.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.hongqiang.shop.common.persistence.BaseDao;
+import com.hongqiang.shop.common.persistence.Page;
+import com.hongqiang.shop.common.utils.Pageable;
+import com.hongqiang.shop.website.entity.Ad;
+
+public abstract interface AdDao extends AdDaoCustom, CrudRepository<Ad, Long> {
+
+}
+
+/**
+ * DAO自定义接口
+ * 
+ * @author Jack
+ * 
+ */
+interface AdDaoCustom extends BaseDao<Ad> {
+
+	public Page<Ad>  findPage(Pageable pageable);
+}
